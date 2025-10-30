7a67f177a19f8556ecc0728f925a1f6ca4cd9b4e
devxyn <johnlawrencehermo@gmail.com>
2025-10-30 18:31:47 +0800

    fix: Correct function name from getSimiliarEventsBySlug to getSimilarEventsBySlug and streamline error handling

--- lib/actions/event.action.ts
+++ lib/actions/event.action.ts
@@ -1,19 +1,15 @@
 "use server";
 
-import Event, { IEvent } from "@/database/event.model";
-import connectDB from "../mongodb";
+import Event from "@/database/event.model";
+import connectDB from "@/lib/mongodb";
 
-export const getSimiliarEventsBySlug = async (slug: string) => {
+export const getSimilarEventsBySlug = async (slug: string) => {
   try {
     await connectDB();
-
     const event = await Event.findOne({ slug });
 
-    const events = await Event.find({ _id: { $ne: event._id }, tags: { $in: event.tags } }).lean();
-
-    return events as unknown as IEvent[];
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  } catch (error) {
+    return await Event.find({ _id: { $ne: event._id }, tags: { $in: event.tags } }).lean();
+  } catch {
     return [];
   }
 };
