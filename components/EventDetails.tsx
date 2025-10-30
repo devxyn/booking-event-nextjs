a540c7e11119647151c79bb80a55c29d1b2e3697
devxyn <johnlawrencehermo@gmail.com>
2025-10-30 19:47:53 +0800

    fix: Improve EventDetails component structure and type definitions

--- components/EventDetails.tsx
+++ components/EventDetails.tsx
@@ -1,10 +1,11 @@
+import React from "react";
+import { notFound } from "next/navigation";
+import { IEvent } from "@/database";
+import { getSimilarEventsBySlug } from "@/lib/actions/event.actions";
+import Image from "next/image";
 import BookEvent from "@/components/BookEvent";
 import EventCard from "@/components/EventCard";
-import { IEvent } from "@/database";
-import { getSimilarEventsBySlug } from "@/lib/actions/event.action";
 import { cacheLife } from "next/cache";
-import Image from "next/image";
-import { notFound } from "next/navigation";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
@@ -15,19 +16,19 @@ const EventDetailItem = ({ icon, alt, label }: { icon: string; alt: string; labe
   </div>
 );
 
-const EventAgenda = ({ agendaItems }: { agendaItems: [string] }) => (
+const EventAgenda = ({ agendaItems }: { agendaItems: string[] }) => (
   <div className='agenda'>
     <h2>Agenda</h2>
     <ul>
-      {agendaItems.map((agenda) => (
-        <li key={agenda}>{agenda}</li>
+      {agendaItems.map((item) => (
+        <li key={item}>{item}</li>
       ))}
     </ul>
   </div>
 );
 
-const EventTags = ({ tags }: { tags: [string] }) => (
-  <div className='flex flex-row gap-5 flex-wrap'>
+const EventTags = ({ tags }: { tags: string[] }) => (
+  <div className='flex flex-row gap-1.5 flex-wrap'>
     {tags.map((tag) => (
       <div className='pill' key={tag}>
         {tag}
@@ -36,15 +37,12 @@ const EventTags = ({ tags }: { tags: [string] }) => (
   </div>
 );
 
-const EventDetails = async ({ params }: { params: Promise<{ slug: string }> }) => {
+const EventDetails = async ({ params }: { params: Promise<string> }) => {
   "use cache";
   cacheLife("hours");
-
-  const { slug } = await params;
-  console.log(params);
+  const slug = await params;
 
   let event;
-
   try {
     const request = await fetch(`${BASE_URL}/api/events/${slug}`, {
       next: { revalidate: 60 },
@@ -68,25 +66,25 @@ const EventDetails = async ({ params }: { params: Promise<{ slug: string }> }) =
     return notFound();
   }
 
-  const { title, description, image, overview, date, time, location, mode, agenda, tags, audience, organizer } = event;
+  const { description, image, overview, date, time, location, mode, agenda, audience, tags, organizer } = event;
 
   if (!description) return notFound();
 
   const bookings = 10;
 
-  const similarEvents: IEvent[] = await getSimilarEventsBySlug(slug);
+  const similarEvents = (await getSimilarEventsBySlug(slug)) as unknown as IEvent[];
 
   return (
     <section id='event'>
       <div className='header'>
         <h1>Event Description</h1>
-        <p className='mt-2'>{description}</p>
+        <p>{description}</p>
       </div>
 
       <div className='details'>
-        {/* Content */}
+        {/*    Left Side - Event Content */}
         <div className='content'>
-          <Image src={image} alt={`${title} Banner`} width={800} height={800} className='banner' />
+          <Image src={image} alt='Event Banner' width={800} height={800} className='banner' />
 
           <section className='flex-col-gap-2'>
             <h2>Overview</h2>
@@ -95,6 +93,7 @@ const EventDetails = async ({ params }: { params: Promise<{ slug: string }> }) =
 
           <section className='flex-col-gap-2'>
             <h2>Event Details</h2>
+
             <EventDetailItem icon='/icons/calendar.svg' alt='calendar' label={date} />
             <EventDetailItem icon='/icons/clock.svg' alt='clock' label={time} />
             <EventDetailItem icon='/icons/pin.svg' alt='pin' label={location} />
@@ -105,13 +104,14 @@ const EventDetails = async ({ params }: { params: Promise<{ slug: string }> }) =
           <EventAgenda agendaItems={agenda} />
 
           <section className='flex-col-gap-2'>
-            <h2>Organizer</h2>
+            <h2>About the Organizer</h2>
             <p>{organizer}</p>
           </section>
 
           <EventTags tags={tags} />
         </div>
-        {/* Form */}
+
+        {/*    Right Side - Booking Form */}
         <aside className='booking'>
           <div className='signup-card'>
             <h2>Book Your Spot</h2>
@@ -130,11 +130,10 @@ const EventDetails = async ({ params }: { params: Promise<{ slug: string }> }) =
         <h2>Similar Events</h2>
         <div className='events'>
           {similarEvents.length > 0 &&
-            similarEvents.map((similarEvent: IEvent) => <EventCard key={similarEvent._id} {...similarEvent} />)}
+            similarEvents.map((similarEvent: IEvent) => <EventCard key={similarEvent.title} {...similarEvent} />)}
         </div>
       </div>
     </section>
   );
 };
-
 export default EventDetails;
