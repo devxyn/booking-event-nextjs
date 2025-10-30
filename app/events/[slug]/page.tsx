9f6f3268ed0c28235c6ad91ae180c08b5a32761a
devxyn <johnlawrencehermo@gmail.com>
2025-10-30 19:47:13 +0800

    fix: Update EventDetailsPage to correctly handle slug extraction and remove unnecessary test text

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,17 +1,15 @@
-import EventDetails from "@/components/EventDetails";
 import { Suspense } from "react";
+import EventDetails from "@/components/EventDetails";
 
 const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
-  const { slug } = await params.then((p) => p.slug);
+  const slug = params.then((p) => p.slug);
 
   return (
     <main>
       <Suspense fallback={<div>Loading...</div>}>
         <EventDetails params={slug} />
-        test
       </Suspense>
     </main>
   );
 };
-
 export default EventDetailsPage;
